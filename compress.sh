1bfacb7f67b541509ffbb2488f2e40ab7ad022c7
spike83 <spike@me.by>
2011-01-07 19:44:28 +0200

    Linking problem still exists

--- compress.sh
+++ compress.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 rm -rf ./build/
-mkdir ../$(date +%Y-%m-%d)
+#mkdir ../$(date +%Y-%m-%d)
+mkdir ../bkup
 
 make clean
 rm SpiceExtract
-tar czvf ../SpiceExtract.tar.gz ../SpiceExtract/
-mv ../SpiceExtract.tar.gz ../$(date +%Y-%m-%d)
+tar czvf ../bkup/SpiceExtract-$(date +%Y-%m-%d).tar.gz ../SpiceExtract/
+#mv ../SpiceExtract.tar.gz ../$(date +%Y-%m-%d)
 #rm -f -r ../SpiceParamExtractor/
 
