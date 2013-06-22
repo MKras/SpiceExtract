e84c11c8bff9266a13ca51469ee71b02c1b5ec1b
spike83 <spike@me.by>
2013-06-22 10:15:18 +0300

    first step

---
+++ core/tests/ng_helper_test.cpp
@@ -0,0 +1,15 @@
+
+
+
+#define BOOST_TEST_MODULE "Unit test example 05"
+#include <boost/test/unit_test.hpp>
+
+
+#include "core/ng_helper.h"
+#include "core/sharedspice.h"
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE(TestThatSucceeds)
+{
+    BOOST_CHECK(true);
+};
