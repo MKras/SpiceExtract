0db25b3fdeef22fb6ba2c3ad3fae60a7ba58027a
spike83 <spike@me.by>
2013-06-22 15:08:49 +0300

    Utils test added

--- util/tests/utils_test.cpp
+++ util/tests/utils_test.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_MODULE stringtest
 #include <boost/test/included/unit_test.hpp>
 #include <boost/filesystem.hpp>
-#include "../ng_helper.h"
+
 
 #include <iostream>
 
@@ -11,13 +11,6 @@ BOOST_AUTO_TEST_CASE (test1)
 {
   std::string s;
   BOOST_CHECK(s.size() == 0);
-
-  NGSpiceWrapper ngw;
-  BOOST_CHECK_NO_THROW (ngw.Init_dll_handler());
-  std::cout<<" ngw.Init_dll_handler();"<<std::endl;
-  BOOST_CHECK_NO_THROW (ngw.Init_handlers());
-  std::cout<<" ngw.Init_handlers();"<<std::endl;
-  BOOST_CHECK_NO_THROW (ngw.NGngSpice_Init_handle());
 }
 
 BOOST_AUTO_TEST_SUITE_END( )
